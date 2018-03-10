e305332d9da97319dac61347eae1c828787c1719
kljohnson2 <kylerljohnson@icloud.com>
2018-03-10 11:46:35 -0500

    Adding functionality to display individual server info on click of servers list item. Doing the same for users

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/Router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-server',
@@ -9,11 +11,20 @@ import { ServersService } from '../servers.service';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
+  id: number;
+  private subscription: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(this.id);        
+    this.subscription = this.route.params.subscribe(params => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
 
 }
